30a61e78e052cacdd1cf3d1f466ed3cbb80f7c26
Neil Brown <neilbrown08@hotmail.com>
2023-05-19 16:49:54 +0100

    update docker

--- src/routes/Finish.tsx
+++ src/routes/Finish.tsx
@@ -10,7 +10,6 @@ export const Finish = ({ room }: Props) => {
   const winners = room?.players.filter((player) => player.score === 5);
   return winners ? (
     <div className={styles.container}>
-      {/* <Header /> */}
       {room?.players?.length && <Players room={room} />}
       <h1>Congratulations to our {winners?.length > 1 ? 'winners' : 'winner'}</h1>
       <ul>
